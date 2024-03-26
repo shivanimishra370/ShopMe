e03d03fb7324d3ed62a88108147ef3b4cd260fa3
shivani <livelycolours510@gmail.com>
2024-03-26 11:35:37 +0530

    first commit

--- src/Customers/Components/Order/OrderDEtails.jsx
+++ src/Customers/Components/Order/OrderDEtails.jsx
@@ -18,14 +18,14 @@ const OrderDEtails = () => {
             <OrderTraker activeStep={3}/>
         </div>
         <Grid className='space-y-5' container>
-            {[1,1,1,1,1].map((item) => (
+            {[1,1,1].map((item) => (
               <Grid item container className='shadow-x1 rounded-md p-5 border' sx={{ alignItems: "center", justifyContent: 'space-between' }}>
                 <Grid item container className='shadow-xl rounded-md p-5 border' sx={{ alignItems: "center", justifyContent: 'space-between' }}>
                   <Grid item xs={6}>
                       <div className='flex items-center space-x-4'>
                           <img className='w-40 h-40 object-cover object-top' src="https://rukminim1.flixcart.com/image/612/612/xif0q/kurta/1/h/g/m-pw333-purshottam-wala-original-imag8zf6ybkmhehy-bb.jpeg?q=70" alt="" />
                           <div className='space-y-2 ml-5'>
-                              <p className='font-semibold'>Men Slim Mid Rise Black Jeans</p>
+                              <p className='font-semibold'>Women's Kurti</p>
                               <p className='space-x-5 opacity-50 text-xs font-semibold'><span>Color: Pink</span><span>Size: M</span></p>
                               <p>Seller: Linaria</p>
                               <p>₹1099</p>
